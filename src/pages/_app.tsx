d88db21f355ddd10031734ce04c7f1c637dc5163
Tookai <tybo.orcel@gmail.com>
2023-10-12 18:44:20 +0200

    feat(setup): project init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import type { AppProps } from "next/app"
+import { Inter } from "next/font/google"
+
+const inter = Inter({ subsets: ["latin"] })
+
+const config = {
+	initialColorMode: "dark",
+	useSystemColorMode: false,
+}
+
+const fonts = {
+	body: inter.style.fontFamily,
+	heading: inter.style.fontFamily,
+}
+
+// 3. extend the theme
+const theme = extendTheme({ config, fonts })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	)
 }

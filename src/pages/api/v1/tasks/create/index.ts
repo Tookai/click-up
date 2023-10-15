c2f93aee0f53a1b22a007d9b7d2c7ba86fa85d0a
Tookai <tybo.orcel@gmail.com>
2023-10-15 13:55:54 +0200

    fix(deploy): prisma client

--- src/pages/api/v1/tasks/create/index.ts
+++ src/pages/api/v1/tasks/create/index.ts
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client"
+import { prisma } from "&/client"
 import { NextApiRequest, NextApiResponse } from "next"
-
-const prisma = new PrismaClient()
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method !== "POST") {
